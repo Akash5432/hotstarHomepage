42ed90c76f26ab960e1c75d5ec693eb867e98432
Akash5432 <ak5244200@gmail.com>
2022-12-06 20:23:05 +0530

    code

--- hotstar.js
+++ hotstar.js
@@ -1,4 +1,11 @@
-const changeMode = () => {
-    let mybody = document.body;
-    mybody.classList.toggle('mydark')
+
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible';
+    document.getElementById('couponMain').style.opacity='0.5';
+}
+
+
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementById('couponMain').style.opacity='1';
 }
