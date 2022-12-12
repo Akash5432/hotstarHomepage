85e179dd8414af2b3db4826a3834ddf8a5b20a9d
Akash5432 <ak5244200@gmail.com>
2022-12-12 11:40:37 +0530

    code

--- hotstar.js
+++ hotstar.js
@@ -4,8 +4,54 @@ function loadCoupon(){
     document.getElementById('couponMain').style.opacity='0.5';
 }
 
-
 function closeCoupon(){
     document.getElementById('coupon').style.visibility = 'hidden';
     document.getElementById('couponMain').style.opacity='1';
 }
+
+let x = document.getElementById('out');
+let y = document.getElementById('weatherOut');
+
+function geolocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }else{
+        x.innerText = "Geo Not Supported"
+    }
+}
+
+function geolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition)
+    } else {
+        x.innerText = "Geo Not Supported"
+    }
+}
+
+function showPosition(data) {
+    console.log(data)
+    let lat = data.coords.latitude;
+    let long = data.coords.longitude;
+    x.innerText = `Latitude is ${lat} and longitude is ${long}`
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${long}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+        //api calling
+    fetch(url, {
+            method: 'GET'
+        })
+        // return promise
+        .then((res) => res.json())
+        // resolve promise
+        .then((data) => {
+            console.log(data)
+            let cityName = data.city.name;
+            let temp = data.list[0].temp.day;
+            y.innerText = `Your city is ${cityName} & temp is ${temp} °C`
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+}
+
+document.getElementById("out").style.color = "white";
+document.getElementById("weatherOut").style.color = "white";
